07b77cbe187e1b2cad31222c43add9fbd6594ae8
Phandal <bfeld.111@gmail.com>
2024-12-01 15:50:05 -0600

    fixed bug with layer 2 switch

--- keyboards/crkbd/keymaps/phandal/keymap.c
+++ keyboards/crkbd/keymaps/phandal/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   KC_ENT,  LT(1,KC_ESC),     LT(2,KC_BSPC),   KC_SPC, KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -46,23 +46,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       QK_BOOT, KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO,                      KC_PGUP, KC_HOME, KC_NO, KC_NO, KC_NO, KC_NO,
+      QK_BOOT,   KC_NO, KC_VOLD, KC_MUTE, KC_VOLU,   KC_NO,                      KC_PGUP, KC_HOME,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,                      KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
+      KC_LCTL,   KC_NO, KC_MPRV, KC_MPLY, KC_MNXT,   KC_NO,                      KC_LEFT,  KC_DOWN,  KC_UP, KC_RGHT,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_PGDN, KC_END, KC_NO, KC_PSCR, KC_NO, KC_NO,
+      KC_LSFT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_PGDN,  KC_END,   KC_NO, KC_PSCR,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   KC_ENT,  LT(3, KC_BSPC),     KC_TRNS, KC_SPC, KC_RALT
+                                          KC_LGUI,   KC_ENT,  LT(3, KC_ESC),     KC_TRNS, KC_SPC, KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
     [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                      KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, KC_ENT, KC_TRNS,     KC_TRNS, KC_SPC, KC_RALT
                                       //`--------------------------'  `--------------------------'
