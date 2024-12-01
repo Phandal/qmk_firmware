1cddbfa0e8fab4401ebb881309c807edbbcf9e83
Phandal <bfeld.111@gmail.com>
2024-12-01 16:12:28 -0600

    home row mods from voyager

--- keyboards/crkbd/keymaps/phandal/keymap.c
+++ keyboards/crkbd/keymaps/phandal/keymap.c
@@ -18,16 +18,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+// Left-hand home row mods
+#define CTL_A LCTL_T(KC_A)
+#define ALT_S LALT_T(KC_S)
+#define SFT_D LSFT_T(KC_D)
+#define GUI_F LGUI_T(KC_F)
+
+// Right-hand home row mods
+#define GUI_J RGUI_T(KC_J)
+#define SFT_K RSFT_T(KC_K)
+#define ALT_L LALT_T(KC_L)
+#define CTL_SCLN RCTL_T(KC_SCLN)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+       KC_ESC,   CTL_A,   ALT_S,   SFT_D,   GUI_F,    KC_G,                         KC_H,   GUI_J,   SFT_K,   ALT_L,CTL_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+        KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   KC_ENT,  LT(1,KC_ESC),     LT(2,KC_BSPC),   KC_SPC, KC_RALT
+                                          KC_LGUI,   KC_ENT,  MO(1),     LT(2,KC_BSPC),   KC_SPC, KC_RALT
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -36,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                         KC_1,    KC_2,   KC_3,     KC_4,    KC_5, KC_UNDS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                      KC_PLUS, KC_MINS,  KC_EQL, KC_ASTR, KC_SLSH,   KC_NO,
+      KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                      KC_PLUS, KC_MINS,  KC_EQL, KC_ASTR, KC_SLSH,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_TILD, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR,                      KC_6,       KC_7,    KC_8,    KC_9,    KC_0,   KC_NO,
+        KC_NO, KC_TILD, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR,                      KC_6,       KC_7,    KC_8,    KC_9,    KC_0,   KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, KC_ENT,  KC_TRNS,     LT(3,KC_BSPC),   KC_SPC, KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -48,11 +60,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       QK_BOOT,   KC_NO, KC_VOLD, KC_MUTE, KC_VOLU,   KC_NO,                      KC_PGUP, KC_HOME,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,   KC_NO, KC_MPRV, KC_MPLY, KC_MNXT,   KC_NO,                      KC_LEFT,  KC_DOWN,  KC_UP, KC_RGHT,   KC_NO,   KC_NO,
+      KC_TRNS,   KC_NO, KC_MPRV, KC_MPLY, KC_MNXT,   KC_NO,                      KC_LEFT,  KC_DOWN,  KC_UP, KC_RGHT,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_PGDN,  KC_END,   KC_NO, KC_PSCR,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_PGDN,  KC_END,   KC_NO, KC_PSCR,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   KC_ENT,  LT(3, KC_ESC),     KC_TRNS, KC_SPC, KC_RALT
+                                          KC_LGUI,   KC_ENT,  MO(3),     KC_TRNS, KC_SPC, KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
