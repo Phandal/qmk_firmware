06400a326f8cc37ea23a2fdf2e817afe01ca075b
Phandal <bfeld.111@gmail.com>
2025-08-18 20:45:54 -0500

    added an extra layer to get the mouse keys working as expected.

--- keyboards/crkbd/keymaps/phandal/config.h
+++ keyboards/crkbd/keymaps/phandal/config.h
@@ -25,8 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 170
 
-#define MK_3_SPEED
-#define MK_MOMENTARY_ACCEL
+#define MK_COMBINED
 
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_EFFECT_BREATHING
